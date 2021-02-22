8f3f2855bcd4dbfbff6292b1269916099aa27021
Wojciech Malarski <w.malarski@gmail.com>
2021-02-22 19:54:50 +0100

    additional configuration

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -2,15 +2,14 @@
  * React renderer.
  */
 // Import the styles here to process them with webpack
-import '_public/style.css';
-
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "_public/style.css";
 
 ReactDOM.render(
   <div className="app">
-    <h4>Welcome to React, Electron and Typescript</h4>
+    <h4>Welcome to React, Electron and Typescript and Lol</h4>
     <p>Hello</p>
   </div>,
-  document.getElementById('app'),
+  document.getElementById("app"),
 );
